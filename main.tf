2d6ac31efedb77575d9780b21d96d17fb8a80a67
friends4433 <sairam.pulagam@gmail.com>
2021-03-05 13:11:39 +0530

    Initial commit

---
+++ main.tf
@@ -0,0 +1,11 @@
+provider "aws" {
+    access_key = "XXXXYYYYZZZ"
+    secret_key = "XXXYYYZZZ"
+    region = "AASWSES"
+  
+}
+
+
+#Added changes for testing
+#Added changes for testing-1
+#Added changes for testing-2
